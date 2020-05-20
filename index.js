0adf3b0998e9c0f5bca39aee49246da9844ee806
Jonelle Alleyne <r.jonellealleyne@gmail.com>
2020-05-20 11:46:54 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+let myVariable;
+myVariable = "Goddess";
+
+myVariable;
+
+myVariable ="God";
+myVariable;
+
+
+
+let iceCream = 'vanilla';
+if(iceCream === 'coconut')//this is the test 
+{
+  alert ('Yay, I love coconut ice cream!')
+}else{
+  alert('Aww, but coconut is my favourite');
+}
+
+alert("Hello Goddess"
+);
+
+
+
+function multiply(num1,num2){
+  let result = num1 * num2;
+  return result;
+}
+
+multiply(3,9);
+
+function multiply(num1,num2){
+  let result = num1 * num2;
+  return result;
+}
+multiply(12,6);
+
+function multiply(num1,num2){
+  let result = num1 * num2;
+  return result;
+}
+multiply(24,36);
+
+
+
